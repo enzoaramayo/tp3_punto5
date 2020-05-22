05853024db390e7c385ceee39654e850f236437f
enzoaramayo <50436437+enzoaramayo@users.noreply.github.com>
2020-05-22 05:01:36 -0300

    commit
    commit

--- obra-social/src/app/app-routing.module.ts
+++ obra-social/src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { InicioComponent } from "./components/inicio/inicio.component";
+import { SliderComponent } from "./components/slider/slider.component";
+import { FormularioComponent } from "./components/formulario/formulario.component";
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'inicio', component: InicioComponent },
+  { path: 'slider', component: SliderComponent },
+  { path: 'formulario', component: FormularioComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'inicio' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- obra-social/src/app/app.module.ts
+++ obra-social/src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/layout/header/header.component';
+import { FooterComponent } from './components/layout/footer/footer.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { SliderComponent } from './components/slider/slider.component';
+import { FormularioComponent } from './components/formulario/formulario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    InicioComponent,
+    SliderComponent,
+    FormularioComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
